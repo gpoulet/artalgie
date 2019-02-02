7943916f7d777b14f3670fad847031848d1ce53f
Gauthier POULET <gauthier.poulet@gmail.com>
2019-02-02 21:07:08 +0100

    fix Footer color and Logo CSS

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { ARTALGIE, PAGE_FACEBOOK, EMAIL, ROUTER, TELEPHONE } from '../../constants'
 import { Link } from 'react-router-dom'
+import Logo from '../Header/Logo'
 
 function LinkFooter({ routing, link }) {
-  return <li key={routing}><Link className="grey-text text-lighten-3" to={routing}>{link}</Link></li>
+  return <li key={routing}><Link className="footer-text" to={routing}>{link}</Link></li>
 }
 
 function Footer() {
@@ -12,15 +13,17 @@ function Footer() {
       <div className="container">
         <div className="row">
           <div className="col l6 s12">
-            <h5 className="white-text">{ARTALGIE}</h5>
-            <p className="grey-text text-lighten-4">
+            <div className="brand-logo">
+            <Logo />
+            </div>
+            <p className="footer-text">
               Téléphone : {TELEPHONE}<br/><br/>
               Adresse e-mail : {EMAIL}<br/><br/>
               Facebook : {PAGE_FACEBOOK}
             </p>
           </div>
           <div className="col l4 offset-l2 s12">
-            <h5 className="white-text">Liens</h5>
+            <h5 className="footer-text">Liens</h5>
             <ul>
               {ROUTER.map(LinkFooter)}
             </ul>
@@ -28,9 +31,9 @@ function Footer() {
         </div>
       </div>
       <div className="footer-copyright">
-        <div className="container">
+        <div className="container footer-text">
           © 2019 {ARTALGIE}
-          <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
+          <a className="footer-text right" href="#!">More Links</a>
         </div>
       </div>
     </footer>);
