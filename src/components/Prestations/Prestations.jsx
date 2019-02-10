125ce020025af5a352c00df2388abdaa0d550ef0
Gauthier POULET <gauthier.poulet@gmail.com>
2019-02-10 20:34:52 +0100

    update page Prestations

--- src/components/Prestations/Prestations.jsx
+++ src/components/Prestations/Prestations.jsx
@@ -8,36 +8,37 @@ function Prestations() {
         <div className="card">
           <div className="card-content">
             <span className="card-title">{LINK_PRESTATIONS}</span>
-            <p>ARTalgie intervient de manière innovante sur deux plans dans votre entreprise : préventif et/ou curatif.
+            <p>ARTalgie intervient de manière innovante sur deux plans dans votre entreprise : préventif et/ou curatif.</p>
+            <p>
               L'innovation tient dans l'approche art-thérapeutique : elle se veut à l'écoute de chaque participant, et
-              dans un parcours actif basé sur les sens.
+              dans un parcours actif basé sur les <u>sens</u>.
             </p>
             <br/>
             <p>
               En <u>prévention</u>, pour améliorer la Qualité de Vie au Travail, ARTalgie propose des sessions comme
               suit :
             </p>
-              <ul>
-                <li> - La cohésion d'équipe</li>
-                <li> - Prendre la parole en public</li>
-                <li> - Gérer le stress</li>
-                <li> - La dynamique de groupe</li>
-                <li> - La confiance en soi</li>
-
-              </ul>
+            <p>
+              - Mieux se connaître pour mieux travailler avec les autres<br/>
+              - Prendre la parole en public<br/>
+              - Prévenir le stress en milieu professionnel<br/>
+              - La confiance en soi<br/>
+              - La cohésion d'équipe<br/>
+              - Accueillir de nouveaux arrivants dans une équipe
+            </p>
             <br/>
             <p>
               En <u>aide</u>, ARTalgie peut intégrer votre plan d'actions contenu dans le document unique, ou suite à un
               diagnostic de risques. Les sessions proposées pourront alors s'intituler :
             </p>
-              <ul>
-                <li>- L'écoute dans le groupe</li>
-                <li>- La communication dans le groupe</li>
-                <li>- L'estime de soi</li>
-                <li>- Décompressez, créez</li>
-                <li>- Lâcher-prise</li>
-
-              </ul>
+            <p>
+              - L'écoute dans le groupe<br/>
+              - La communication dans le groupe<br/>
+              - Dépasser les conflits<br/>
+              - L'estime de soi<br/>
+              - Décompressez, créez<br/>
+              - Lâcher-prise
+            </p>
             <br/>
             <p>
               Plusieurs outils art-thérapeutiques peuvent être envisagés et mixés : théâtre, danse, peinture, terre,
@@ -47,12 +48,14 @@ function Prestations() {
             <p>
               Les interventions se font dans vos locaux par demi-journée, pour des groupes de maximum 10 personnes. Le
               matériel spécifique est fourni.
-              Le tarif de base est fixé à 800€ pour une demi-journée.
+            </p>
+            <p>Le tarif de base est fixé à 800€ pour une demi-journée.
             </p>
             <p> Il ne comprend pas les frais de déplacement, ou une éventuelle location de salle. Un devis sur mesure
               sera établi selon vos besoins.
-
             </p>
+            <br/>
+            <p><b>Un devis sur mesure sera établi pour répondre à vos spécificités.</b></p>
           </div>
         </div>
       </div>
