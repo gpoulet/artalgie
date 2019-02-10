772b9520f9500f70a5773b3e140fe4205968dc74
Gauthier POULET <gauthier.poulet@gmail.com>
2019-02-10 12:03:32 +0100

    update Reseau

--- src/components/Reseau/Reseau.js
+++ src/components/Reseau/Reseau.js
@@ -2,74 +2,48 @@ import React from "react";
 import { LINK_RESEAU } from '../../constants'
 
 function Reseau() {
-  return (<div className="row">
+  return (
+    <div className="row">
       <div className="col s12">
         <div className="card">
           <div className="card-content">
             <span className="card-title">{LINK_RESEAU}</span>
 
-            <p>
-              Concernant l'art-thérapie :<br/>
-              ARTEEP<br/>
-              http://www.arteep.fr/
-            </p>
-
-            <p>FFAT : <br/>
-              http://ffat-federation.org</p>
-
-            <br/>
-            <p>Wikipédia :<br/>
-              https://fr.wikipedia.org/wiki/Art-thérapie</p>
-            <br/>
-
-            <p><b>Concernant les risques psychosociaux :</b><br/>
-              Ministère du travail et de l'emploi<br/>
-              https://travail-emploi.gouv.fr/sante-au-travail/prevention-des-risques-pour-la-sante-au-travail/article/les-rps-c-est-quoi
-            </p>
-
-            <br/>
-            <p>Anact :<br/>
-              https://www.anact.fr/themes/risques-psychosociaux</p>
-            <br/>
-
-            <p>Ameli :<br/>
-              https://www.ameli.fr/llle-et-vilaine/entreprise/prevention/risques/risques-psychosociaux</p>
-            <br/>
-            <p>INRS<br/>
-              http://www.inrs.fr/risques/psychosociaux/ce-qu-il-faut-retenir.html</p>
-
-
-            <p>Photographe :<br/>
-              Instagram<br/>
-              https://www.instagram.com/clairelefourn/?hl=fr</p>
-
-            <p>Facebook<br/>
-              https://www.facebook.com/clairelefourn/
-            </p>
-            <br/>
-
-            <p>LinkedIn<br/>
-              https://www.linkedin.com/in/claire-le-fourn/?originalSubdomain=fr</p>
-
-
-            <p>Le site de l'intervenante :<br/>
-              Nathalie POULET-GUILBERT<br/>
-              http://nathaliepoulet.com</p>
-
-            <br/>
-            <p>A visiter…<br/>
-              AQT<br/>
-              http://www.quaisetoiles.fr</p>
+            <p>Je vous invite à prendre le temps de ces visites, à les rencontrer.</p>
             <br/>
             <p>
-              Alexandra SERINGE<br/>
-              http://www.alexandraseringe.com<br/></p>
+              Vous pourrez découvrir leurs actions, leurs activités, leurs motivations…</p>
+            <br/>
+
+            <p>
+              <a href="http://www.quaisetoiles.fr" target="_blank" rel="noopener noreferrer">
+                AQT</a>
+            </p>
+            <br/>
+
+            <p>
+              <a href="http://www.alexandraseringe.com" target="_blank" rel="noopener noreferrer">
+                Alexandra SERINGE</a>
+            </p>
+            <br/>
+
+            <p>
+              <a href="http://valerie-leroux.fr" target="_blank" rel="noopener noreferrer">
+                Valérie LE ROUX</a>
+            </p>
             <br/>
             <p>
-              Valérie LE ROUX<br/>
-              http://valerie-leroux.fr
+              La photographe :<br/>
+              <a href="https://www.instagram.com/clairelefourn/?hl=fr" target="_blank" rel="noopener noreferrer">
+                Instagram</a>
+              <br/>
+              <a href="https://www.facebook.com/clairelefourn/" target="_blank" rel="noopener noreferrer">
+                Facebook</a>
+              <br/>
+              <a href="https://www.linkedin.com/in/claire-le-fourn/?originalSubdomain=fr" target="_blank"
+                 rel="noopener noreferrer">
+                LinkedIn</a>
             </p>
-
 
           </div>
         </div>
