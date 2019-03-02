3a9e9b3de0c5b11fae79284d85c9b02782f2b904
Gauthier POULET <gauthier.poulet@gmail.com>
2019-03-02 14:13:26 +0100

    fix temporary facebook url

--- src/constants/wording.js
+++ src/constants/wording.js
@@ -4,6 +4,6 @@ export const ALGIE = "algie";
 
 export const TELEPHONE = "06 59 23 18 95";
 export const EMAIL = "contact@artalgie.fr";
-export const PAGE_FACEBOOK = "https://www.facebook.com/ARTalgie/";
+export const PAGE_FACEBOOK = "https://www.facebook.com/nathalie.artalgie";
 
 export const SITE_NATHALIE = "http://www.nathaliepoulet.com"
