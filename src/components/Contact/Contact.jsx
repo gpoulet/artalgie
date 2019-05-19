99edc3fdcff5c5f830a67eec3a64ae2baa861e9a
Gauthier POULET <gauthier.poulet@gmail.com>
2019-05-19 22:48:49 +0200

    improve Contact page : fix design bug on mobile

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -17,16 +17,22 @@ function Contact() {
   return (
     <div className="row">
       <div className="col s12">
-        <div className="card horizontal">
+        <div className="card horizontal hide-on-med-and-down">
           <div className="card-stacked">
             <div className="card-content contact-card-content">
               <span className="card-title">{LINK_CONTACT}</span>
               {CONTACTS.map((contact, index) => <Field key={index} {...contact}/>)}
             </div>
           </div>
-          <div className="card-image card-image-contact hide-on-med-and-down">
+          <div className="card-image card-image-contact">
             <img src="/images/contact.jpg" alt="contact"/>
           </div>
+        </div>
+        <div className="card hide-on-large-only hide-on-extra-large-only">
+            <div className="card-content contact-card-content">
+              <span className="card-title">{LINK_CONTACT}</span>
+              {CONTACTS.map((contact, index) => <Field key={index} {...contact}/>)}
+            </div>
         </div>
       </div>
     </div>
