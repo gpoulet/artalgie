a45e6f98da4241e3161cdd24fff3df8a407ec373
Gauthier POULET <gauthier.poulet@gmail.com>
2021-09-11 23:24:10 +0200

    fix lighthouse

--- src/components/Footer/Field.tsx
+++ src/components/Footer/Field.tsx
@@ -9,7 +9,7 @@ export interface FieldProps {
 const Field = ({contact: {icon, label, link, value, target}}: FieldProps): ReactElement =>
   <p className="valign-wrapper"><i className="material-icons contact">{icon}</i>
     {label}&nbsp;:&nbsp;
-    <a href={link} target={target} className="truncate">{value}</a>
+    <a href={link} target={target} className="truncate" rel="noopener noreferrer">{value}</a>
   </p>
 
 
