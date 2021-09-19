7d78f57360972f541c3b70a152a56e47692755e4
Gauthier POULET <gauthier.poulet@gmail.com>
2021-09-19 12:50:47 +0200

    add images for cabinet

---
+++ src/components/Cabinet/Cabinet.constants.ts
@@ -0,0 +1,43 @@
+export const CABINET_PHOTOS = [
+  {
+    src: '/images/cabinet/cabinet.jpg',
+    width: 4,
+    height: 3
+  },
+  {
+    src: '/images/cabinet/cabinet2.jpg',
+    width: 576,
+    height: 557
+  },
+  {
+    src: '/images/cabinet/cabinet8.jpg',
+    width: 4,
+    height: 3
+  },
+  {
+    src: '/images/cabinet/cabinet7.jpg',
+    width: 4,
+    height: 3
+  },
+
+  {
+    src: '/images/cabinet/cabinet4.jpg',
+    width: 4,
+    height: 3
+  },
+  {
+    src: '/images/cabinet/cabinet5.jpg',
+    width: 3,
+    height: 4
+  },
+  {
+    src: '/images/cabinet/cabinet3.jpg',
+    width: 1984,
+    height: 2555
+  },
+  {
+    src: '/images/cabinet/cabinet6.jpg',
+    width: 4,
+    height: 3
+  },
+]
--- src/components/Cabinet/Cabinet.tsx
+++ src/components/Cabinet/Cabinet.tsx
@@ -3,31 +3,30 @@ import React, { memo, ReactElement } from "react";
 import { ADDRESS_CABINET, ADDRESS_LINK, LINK_CABINET } from '../../constants'
 
 import "./_cabinet.scss";
+import Gallery from "react-photo-gallery";
+import { CABINET_PHOTOS } from "./Cabinet.constants";
 
 const Cabinet = (): ReactElement =>
   <div className="row no-margin-bottom">
     <div className="col s12">
-      <div className={window.innerWidth > 960 ? "card horizontal" : "card"}>
-        <div className="card-stacked">
-          <div className="card-content">
-            <span className="card-title">{LINK_CABINET}</span>
-            <p>
-              Nathalie vous reçoit dans son <b>cabinet</b> situé au <i>
-              <a href={ADDRESS_LINK}
-                 target="_blank"
-                 rel="noreferrer">
-                {ADDRESS_CABINET}
-              </a>
-            </i>.
-            </p>
-            <br/>
-            <p>Elle vous accueille pour des <b>atelier en groupe</b> ou en <b>individuel</b>.</p>
-            <br/>
-            <p>Il est possible de se faire <b>rembourser</b> des séances. Renseignez-vous auprès de votre mutuelle.</p>
-          </div>
-        </div>
-        <div className="card-image card-image-cabinet">
-          <img src="/images/cabinet/cabinet.jpg" alt="cabinet"/>
+      <div className="card">
+        <div className="card-content">
+          <span className="card-title">{LINK_CABINET}</span>
+          <p>
+            Nathalie vous reçoit dans son <b>cabinet</b> situé au <i>
+            <a href={ADDRESS_LINK}
+               target="_blank"
+               rel="noreferrer">
+              {ADDRESS_CABINET}
+            </a>
+          </i>.
+          </p>
+          <br/>
+          <p>Elle vous accueille pour des <b>atelier en groupe</b> ou en <b>individuel</b>.</p>
+          <br/>
+          <p>Il est possible de se faire <b>rembourser</b> des séances. Renseignez-vous auprès de votre mutuelle.</p>
+          <br/>
+          <Gallery photos={CABINET_PHOTOS}/>
         </div>
       </div>
     </div>
